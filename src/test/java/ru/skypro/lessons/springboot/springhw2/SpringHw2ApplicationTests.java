2faa922717b6584e78e1140cc493b4fd7fe757d2
Macintosh <rassadinmisha714@gmail.com>
2023-05-29 17:23:22 +0300

    Initial commit

---
+++ src/test/java/ru/skypro/lessons/springboot/springhw2/SpringHw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.springhw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHw2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
